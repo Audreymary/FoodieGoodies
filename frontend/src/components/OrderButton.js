75e1213b9ca8ff68e5d96425b3378e176f1cf3b6
Audreymary <audreymoraa006@gmail.com>
2025-02-02 22:05:32 +0300

    add initial commit

--- frontend/src/components/OrderButton.js
+++ frontend/src/components/OrderButton.js
@@ -1,29 +1,3 @@
-// // src/components/OrderButton.js
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// function OrderButton({ onOrder }) {
-//   const handleOrderClick = () => {
-//     if (typeof onOrder === 'function') {
-//       onOrder();  // Calls the onOrder function passed as a prop
-//     } else {
-//       console.error("onOrder is not a function");
-//     }
-//   };
-
-//   return (
-//     <button onClick={handleOrderClick}>Order Now</button>
-//   );
-// }
-
-// // Ensure that onOrder is passed as a function
-// OrderButton.propTypes = {
-//   onOrder: PropTypes.func.isRequired,
-// };
-
-// export default OrderButton;
-
-
 import React from 'react';
 
 function OrderButton({ onOrder }) {
